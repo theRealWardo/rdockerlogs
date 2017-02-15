47388af294724e716b46a4d3a593e68f4bf86ea1
Matt Ward <contact@mwdesigns.com>
2017-02-15 15:50:16 -0800

    start talking to remote machines

--- host/host.go
+++ host/host.go
@@ -1,5 +1,87 @@
 package host
 
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"os/exec"
+	"strings"
+
+	"github.com/jeromer/syslogparser/rfc3164"
+)
+
+const DOCKER_TAG = "docker/"
+
 type Host struct {
-	Ip string
+	Ip            string
+	User          string
+	IdentityFile  string
+	InspectFormat string
+	containers    map[string]string
+}
+
+func (h *Host) cmd(cmd string) *exec.Cmd {
+	return exec.Command("ssh", fmt.Sprintf("%s@%s", h.User, h.Ip), "-i", h.IdentityFile, cmd)
+}
+
+func (h *Host) resolveContainer(id string) string {
+	if value, ok := h.containers[id]; ok {
+		return value
+	}
+	cmd := h.cmd(fmt.Sprintf("docker inspect -f '%s' %s", h.InspectFormat, id))
+	var stdout bytes.Buffer
+	cmd.Stdout = &stdout
+	cmd.Run()
+	l, _ := stdout.ReadString('\n')
+	lines := strings.Split(l, "\n")
+	l = lines[0]
+	if h.containers == nil {
+		h.containers = make(map[string]string)
+	}
+	if len(l) > 0 {
+		h.containers[id] = l
+		return l
+	}
+	h.containers[id] = id
+	return id
+}
+
+func (h *Host) WriteLines(out chan string, data string) {
+	lines := strings.Split(data, "\n")
+	for _, line := range lines {
+		if len(line) > 0 {
+			// Hack in the priority...
+			p := rfc3164.NewParser([]byte("<34>" + line))
+			err := p.Parse()
+			if err != nil {
+				out <- fmt.Sprintf("%s E - %s", h.Ip, err)
+				out <- fmt.Sprintf("%s   - %s", h.Ip, line)
+				continue
+			}
+			d := p.Dump()
+			tag := d["tag"].(string)
+			if strings.Index(tag, DOCKER_TAG) == 0 {
+				tag = h.resolveContainer(tag[len(DOCKER_TAG):])
+			}
+			out <- fmt.Sprintf("%s [%s] - %s", h.Ip, tag, d["content"])
+		}
+	}
+}
+
+func (h *Host) Logs(out chan string) {
+	cmd := h.cmd("tail -f /var/log/syslog")
+	var stdout bytes.Buffer
+	cmd.Stdout = &stdout
+	var stderr bytes.Buffer
+	cmd.Stderr = &stderr
+	err := cmd.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		l, _ := stdout.ReadString('\n')
+		h.WriteLines(out, l)
+		l, _ = stderr.ReadString('\n')
+		h.WriteLines(out, l)
+	}
 }
--- main.go
+++ main.go
@@ -4,12 +4,15 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"os"
+	"os/signal"
 	"strings"
 
 	"github.com/therealwardo/rdockerlogs/host"
 )
 
 var hostStr = flag.String("hosts", "", "Hosts to tail logs on.")
+var userStr = flag.String("user", "", "The username to login to the host as.")
 var inspectFormat = flag.String("format", "{{.Name}}", "The format to pass to docker inspect, used for the first block of each log line.")
 var rancherFile = flag.String("rancher", "", "The Rancher config file to use if talking to Rancher.")
 var rancherService = flag.String("service", "", "The Rancher service to tail logs for.")
@@ -18,6 +21,7 @@ var configFile = flag.String("config", "", "A config file to use instead of flag
 
 type ConfigData struct {
 	hosts         []string
+	user          string
 	inspectFormat string
 	rancherFile   string
 	// rancherConfigData *rancher.ConfigData
@@ -29,6 +33,9 @@ func buildConfigData() *ConfigData {
 	if len(*configFile) > 0 {
 		if len(*hostStr) > 0 {
 			log.Fatal("-hosts cannot be specified with -config")
+		}
+		if len(*userStr) > 0 {
+			log.Fatal("-user cannot be specified with -config")
 		}
 		if len(*inspectFormat) > 0 {
 			log.Fatal("-format cannot be specified with -config")
@@ -51,6 +58,9 @@ func buildConfigData() *ConfigData {
 	if len(*hostStr) > 0 {
 		config.hosts = strings.Split(*hostStr, ",")
 	}
+	if len(*userStr) > 0 {
+		config.user = *userStr
+	}
 	if len(*inspectFormat) > 0 {
 		config.inspectFormat = *inspectFormat
 	}
@@ -69,9 +79,18 @@ func buildConfigData() *ConfigData {
 
 func resolveHosts(config *ConfigData) []*host.Host {
 	hosts := make([]*host.Host, 0)
+	longestIp := 0
+	for _, ip := range config.hosts {
+		if len(ip) > longestIp {
+			longestIp = len(ip)
+		}
+	}
 	for _, ip := range config.hosts {
 		hosts = append(hosts, &host.Host{
-			Ip: ip,
+			Ip:            ip,
+			User:          config.user,
+			IdentityFile:  config.identityFile,
+			InspectFormat: config.inspectFormat,
 		})
 	}
 	return hosts
@@ -81,9 +100,31 @@ func main() {
 	flag.Parse()
 
 	config := buildConfigData()
+	// Prepare the output channel.
+	output := make(chan string)
+	// Close connections channel.
+	close := make(chan bool)
+	// Listen for SIGINT to tell us to close down.
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		for _ = range c {
+			close <- true
+		}
+	}()
 	// Resolve hosts to connect to.
 	hosts := resolveHosts(config)
 	for _, h := range hosts {
-		fmt.Printf("%+v\n\n", h)
+		// Pull logs from the host.
+		go h.Logs(output)
+	}
+	for {
+		select {
+		case data := <-output:
+			fmt.Println(data)
+		case <-close:
+			fmt.Println("\n\nSIGINT received, shutting down...")
+			return
+		}
 	}
 }
